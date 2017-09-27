593291a95fc9e76bf969f5a31926d7fe9d346ffa
Mikalai Sokarau <nklskr@gmail.com>
2017-09-27 16:52:47 +0300

    Added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  return str.split('').reduce((acc, sign) => {
+    const pair = bracketsConfig.filter(arr => sign === arr[0] || sign === arr[1]),
+          openingSign = pair[0][0],
+          closingSign = pair[0][1],
+          lastAccSign = acc[acc.length - 1];
+
+    if (sign === openingSign && sign !== closingSign) {
+      acc.push(sign);
+    } else if (sign === closingSign && openingSign === lastAccSign) {
+      acc.pop();
+    } else {
+      acc.push(sign);
+    }
+    return acc;
+  }, []).length === 0;
+};
